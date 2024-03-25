91bd5bc345e89952f834f1228211e48110c60165
joaberch <122774888+joaberch@users.noreply.github.com>
2024-03-25 15:45:34 +0100

    start the home menu

---
+++ Program/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //MainPage = new NavigationPage(new MainPage());
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new chooseUser());
+        }
+    }
+
+}
